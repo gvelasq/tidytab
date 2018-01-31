6e1333bf7f606d87aabe3d1dc29cfd381a2d1643
gvelasq2 <gvelasq2github@gmail.com>
2018-01-31 18:17:11 +0200

    document br() and %gin%

---
+++ R/br.R
@@ -0,0 +1,14 @@
+#' Browse data
+#'
+#' @description `br()` is an alias for `utils::View()` and invokes the data viewer. See \code{utils::\link[View]{View()}} for details.
+#'
+#' @usage
+#' br(x, title)
+#'
+#' @param x An `R` object coercible into a data frame.
+#' @param title Optional title for viewer window.
+#'
+#' @export
+br <- function(x, title) {
+  View(x, title)
+}
---
+++ R/gin.R
@@ -0,0 +1,29 @@
+#' A reimagination of \code{\%in\%} for partial string matching
+#'
+#' @name %gin%
+#' @rdname gin
+#' @rdname \%gin\%
+#'
+#' @description
+#' \code{\%gin\%} is a reimagination of \code{\%in\%} using `grepl()` for partial string matching.
+#'
+#' @usage
+#' pattern \%gin\% x
+#'
+#' @param pattern Character string to be matched.
+#' @param x `R` object to be matched against. The object must be a character vector or an object coercible by `as.character()` to a character vector.
+#'
+#' @examples
+#' # %in% evaluates to FALSE because it looks for full string matches
+#' "t" %in% "tonic"
+#'
+#' # %gin% evaluates to TRUE
+#' "t" %gin% "tonic"
+#'
+#' @references
+#' \code{\%gin\%} was first written for [@ivelasq](https://github.com/ivelasq)'s [r-data-recipes](https://github.com/ivelasq/r-data-recipes) GitHub repository.
+#'
+#' @export
+"%gin%" <- function(pattern, x) {
+  grepl(pattern, x)
+}
