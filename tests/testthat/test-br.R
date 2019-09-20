d960990efbbc507f3c9212fac1ca6fb4ed38a951
Gustavo Velásquez <gvelasq2github@gmail.com>
2019-09-20 17:47:07 -0400

    Update br() unit test

--- tests/testthat/test-br.R
+++ tests/testthat/test-br.R
@@ -5,7 +5,7 @@ context("test-br.R")
 
 test_that("br() throws an error when not given an input", {
   expect_error(br())
-  expect_error(br(NULL, title = "test"))
+  expect_error(br(title = "test"))
 })
 
 test_that("br() invisibly returns its input", {
