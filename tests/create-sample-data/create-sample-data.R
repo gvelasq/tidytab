8a952f8d0129eca0dc4adc0c7553f52f1fff95fa
Gustavo Velásquez <gvelasq2github@gmail.com>
2021-04-02 16:11:26 -0700

    Fix tests

--- tests/create-sample-data/create-sample-data.R
+++ tests/create-sample-data/create-sample-data.R
@@ -1,7 +1,8 @@
-# this R script creates sample tables for unit testing
-# tables last created on: 20Mar18 (tabr version 0.0.0.9000)
+# create sample tables for unit testing
+# tables last created on: 2021-04-02 (tidytab version 0.0.0.9000)
 
-# setup
+# 0. setup ----
+library(cli)
 library(dplyr)
 library(here)
 library(readr)
@@ -9,48 +10,119 @@ library(stringr)
 library(tidytab)
 library(utils)
 
-# a. demonstrate 'oneway' flavor for one-way tables of frequencies
-a <- mtcars %>% count(gear) %>% rename(Freq. = n)
-a <- a %>% add_row(gear = "Total", Freq. = sum(a[, 2]))
-table_a <- capture.output(statascii(a, flavor = "oneway"))
-
-# b. demonstrate 'oneway' flavor with no padding
-b <- mtcars %>% count(gear) %>% rename(Freq. = n)
-b <- b %>% add_row(gear = "Total", Freq. = sum(b[, 2]))
-table_b <- capture.output(statascii(b, flavor = "oneway", padding = "none"))
-
-# c. demonstrate 'twoway' flavor for n-way tables of frequencies
-c <- mtcars %>% count(gear, carb, am) %>% rename(Freq. = n)
-c <- c %>% ungroup() %>% add_row(gear = "Total", carb = "", am = "", Freq. = sum(c[, 4]))
-table_c <- capture.output(statascii(c, flavor = "twoway"))
-
-# d. demonstrate 'twoway' flavor with dashed group separator
-d <- mtcars %>% count(gear, carb, am) %>% rename(Freq. = n)
-d <- d %>% ungroup() %>% add_row(gear = "Total", carb = "", am = "", Freq. = sum(d[, 4]))
-table_d <- capture.output(statascii(d, flavor = "twoway", separators = TRUE))
-
-# e. demonstrate 'summary' flavor for summary statistics
-e <- mtcars %>% group_by(gear) %>% summarize(
-  Obs = n(),
-  Mean = mean(gear),
-  "Std. Dev." = sd(gear),
-  Min = min(gear),
-  Max = max(gear)
-)
-table_e <- capture.output(statascii(e, flavor = "summary", padding = "summary"))
-
-# f. demonstrate wrapping feature for wide tables
-f <- mtcars %>%
-  mutate(cyl2 = cyl, vs2 = vs, am2 = am, carb2 = carb) %>%
-  filter(gear != 5) %>%
-  count(gear, carb, am, vs, cyl, carb2, am2, vs2, cyl2) %>%
+# 1. demonstrate 'oneway' flavor for one-way tables of frequencies ----
+a <-
+  mtcars %>%
+  count(gear) %>%
   rename(Freq. = n) %>%
+  mutate(gear = as.character(gear))
+a <-
+  a %>%
+  add_row(gear = "Total", Freq. = sum(a[, 2]))
+table_a <-
+  capture.output(tidytab:::statascii(a, flavor = "oneway"))
+
+# 2. demonstrate 'oneway' flavor with no padding ----
+b <-
+  mtcars %>%
+  count(gear) %>%
+  rename(Freq. = n) %>%
+  mutate(gear = as.character(gear))
+b <-
+  b %>%
+  add_row(gear = "Total", Freq. = sum(b[, 2]))
+table_b <-
+  capture.output(tidytab:::statascii(b, flavor = "oneway", padding = "none"))
+
+# 3. demonstrate 'twoway' flavor for n-way tables of frequencies ----
+c <-
+  mtcars %>%
+  count(gear, carb, am) %>%
+  rename(Freq. = n) %>%
+  mutate(
+    gear = as.character(gear),
+    carb = as.character(carb),
+    am = as.character(am)
+  )
+c <-
+  c %>%
+  add_row(
+    gear = "Total",
+    carb = "",
+    am = "",
+    Freq. = sum(c[, 4])
+  )
+table_c <-
+  capture.output(tidytab:::statascii(c, flavor = "twoway"))
+
+# 4. demonstrate 'twoway' flavor with dashed group separator ----
+d <-
+  mtcars %>%
+  count(gear, carb, am) %>%
+  rename(Freq. = n) %>%
+  mutate(
+    gear = as.character(gear),
+    carb = as.character(carb),
+    am = as.character(am)
+  )
+d <-
+  d %>%
+  add_row(
+    gear = "Total",
+    carb = "",
+    am = "",
+    Freq. = sum(d[, 4])
+  )
+table_d <-
+  capture.output(tidytab:::statascii(d, flavor = "twoway", separators = TRUE))
+
+# 5. demonstrate 'summary' flavor for summary statistics ----
+e <-
+  mtcars %>%
+  group_by(gear) %>%
+  summarize(
+    Obs = n(),
+    Mean = mean(gear),
+    "Std. Dev." = sd(gear),
+    Min = min(gear),
+    Max = max(gear)
+  )
+table_e <-
+  capture.output(tidytab:::statascii(e, flavor = "summary", padding = "summary"))
+
+# 6. demonstrate wrapping feature for wide tables ----
+f <-
+  mtcars %>%
+  mutate(
+    cyl2 = cyl,
+    vs2 = vs,
+    am2 = am,
+    carb2 = carb
+  ) %>%
+  filter(gear != 5) %>%
+  count(
+    gear,
+    carb,
+    am,
+    vs,
+    cyl,
+    carb2,
+    am2,
+    vs2,
+    cyl2
+  ) %>%
+  rename(Freq. = n) %>%
+  mutate(gear = as.character(gear)) %>%
   ungroup()
-f <- f %>% add_row(gear = "Total", Freq. = sum(f[, 10]))
+f <-
+  f %>%
+  add_row(gear = "Total", Freq. = sum(f[, 10]))
 f[is.na(f)] <- ""
 options("width" = 80)
-table_f <- capture.output(statascii(f, flavor = "oneway", separators = TRUE))
+table_f <-
+  capture.output(tidytab:::statascii(f, flavor = "oneway", separators = TRUE))
 
-# create list of tables and output to sample-data folder
+# 7. create list of tables and output to sample-data folder ----
 tables <- list(table_a, table_b, table_c, table_d, table_e, table_f)
+tables <- lapply(tables, ansi_strip)
 write_rds(tables, here("tests", "testthat", "sample-tables.rds"))
--- tests/testthat/test-statascii.R
+++ tests/testthat/test-statascii.R
@@ -1,3 +1,4 @@
+library(cli)
 library(dplyr)
 library(here)
 library(readr)
@@ -18,55 +19,145 @@ test_that("data frame must have at least three columns for 'twoway' flavor", {
 
 test_that("oneway flavor works", {
   skip_on_appveyor()
-  a <- mtcars %>% count(gear) %>% rename(Freq. = n)
-  a <- a %>% add_row(gear = "Total", Freq. = sum(a[, 2]))
-  expect_equal(tables[[1]], utils::capture.output(statascii(a, flavor = "oneway")))
+  a <-
+    mtcars %>%
+    count(gear) %>%
+    rename(Freq. = n) %>%
+    mutate(gear = as.character(gear))
+  a <-
+    a %>%
+    add_row(gear = "Total", Freq. = sum(a[, 2]))
+  expect_equal(
+    tables[[1]],
+    ansi_strip(
+      capture.output(tidytab:::statascii(a, flavor = "oneway"))
+    )
+  )
 })
 
 test_that("oneway flavor with no padding works", {
   skip_on_appveyor()
-  b <- mtcars %>% count(gear) %>% rename(Freq. = n)
-  b <- b %>% add_row(gear = "Total", Freq. = sum(b[, 2]))
-  expect_equal(tables[[2]], utils::capture.output(statascii(b, flavor = "oneway", padding = "none")))
+  b <-
+    mtcars %>%
+    count(gear) %>%
+    rename(Freq. = n) %>%
+    mutate(gear = as.character(gear))
+  b <-
+    b %>%
+    add_row(gear = "Total", Freq. = sum(b[, 2]))
+  expect_equal(
+    tables[[2]],
+    ansi_strip(
+      capture.output(statascii(b, flavor = "oneway", padding = "none"))
+    )
+  )
 })
 
 test_that("twowway flavor works for 3-way table", {
   skip_on_appveyor()
-  c <- mtcars %>% count(gear, carb, am) %>% rename(Freq. = n)
-  c <- c %>% ungroup() %>% add_row(gear = "Total", carb = "", am = "", Freq. = sum(c[, 4]))
-  expect_equal(tables[[3]], utils::capture.output(statascii(c, flavor = "twoway")))
+  c <-
+    mtcars %>%
+    count(gear, carb, am) %>%
+    rename(Freq. = n) %>%
+    mutate(
+      gear = as.character(gear),
+      carb = as.character(carb),
+      am = as.character(am)
+    )
+  c <-
+    c %>%
+    add_row(
+      gear = "Total",
+      carb = "",
+      am = "",
+      Freq. = sum(c[, 4])
+    )
+  expect_equal(
+    tables[[3]],
+    ansi_strip(
+      capture.output(statascii(c, flavor = "twoway"))
+    )
+  )
 })
 
 test_that("twoway flavor works with dashed group separators", {
   skip_on_appveyor()
-  d <- mtcars %>% count(gear, carb, am) %>% rename(Freq. = n)
-  d <- d %>% ungroup() %>% add_row(gear = "Total", carb = "", am = "", Freq. = sum(d[, 4]))
-  expect_equal(tables[[4]], utils::capture.output(statascii(d, flavor = "twoway", separators = TRUE)))
+  d <-
+    mtcars %>%
+    count(gear, carb, am) %>%
+    rename(Freq. = n) %>%
+    mutate(
+      gear = as.character(gear),
+      carb = as.character(carb),
+      am = as.character(am)
+    )
+  d <-
+    d %>%
+    add_row(
+      gear = "Total",
+      carb = "",
+      am = "",
+      Freq. = sum(d[, 4])
+    )
+  expect_equal(
+    tables[[4]],
+    ansi_strip(
+      capture.output(statascii(d, flavor = "twoway", separators = TRUE))
+    )
+  )
 })
 
 test_that("summary flavor with summary padding works", {
   skip_on_appveyor()
-  e <- mtcars %>% group_by(gear) %>% summarize(
-    Obs = n(),
-    Mean = mean(gear),
-    "Std. Dev." = sd(gear),
-    Min = min(gear),
-    Max = max(gear)
+  e <-
+    mtcars %>%
+    group_by(gear) %>%
+    summarize(
+      Obs = n(),
+      Mean = mean(gear),
+      "Std. Dev." = sd(gear),
+      Min = min(gear),
+      Max = max(gear)
+    )
+  expect_equal(
+    tables[[5]],
+    ansi_strip(
+      capture.output(statascii(e, flavor = "summary", padding = "summary"))
+    )
   )
-  expect_equal(tables[[5]], utils::capture.output(statascii(e, flavor = "summary", padding = "summary")))
 })
 
 test_that("wrap_tbl() works", {
   skip_on_appveyor()
-  # f. demonstrate wrapping feature for wide tables
   f <- mtcars %>%
-    mutate(cyl2 = cyl, vs2 = vs, am2 = am, carb2 = carb) %>%
+    mutate(
+      cyl2 = cyl,
+      vs2 = vs,
+      am2 = am,
+      carb2 = carb
+    ) %>%
     filter(gear != 5) %>%
-    count(gear, carb, am, vs, cyl, carb2, am2, vs2, cyl2) %>%
+    count(
+      gear,
+      carb,
+      am,
+      vs,
+      cyl,
+      carb2,
+      am2,
+      vs2,
+      cyl2
+    ) %>%
     rename(Freq. = n) %>%
+    mutate(gear = as.character(gear)) %>%
     ungroup()
   f <- f %>% add_row(gear = "Total", Freq. = sum(f[, 10]))
   f[is.na(f)] <- ""
   options("width" = 80)
-  expect_equal(tables[[6]], utils::capture.output(statascii(f, flavor = "oneway", separators = TRUE)))
+  expect_equal(
+    tables[[6]],
+    ansi_strip(
+      capture.output(statascii(f, flavor = "oneway", separators = TRUE))
+    )
+  )
 })
